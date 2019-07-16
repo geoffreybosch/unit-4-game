2ae2d395b73f998a3f7af0a1963e58a707b4abcf
Geoffrey Bosch <49883803+geoffreybosch@users.noreply.github.com>
2019-07-16 16:37:35 -0700

    Updated math for crystal values and condensed code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ var answerC3 = "";
 var answerC4 = "";
 
 function updateAnswer(){
-    answer = parseInt([Math.floor(Math.random() * 100)]);
+    answer = parseInt([Math.floor(Math.random() * 102)]+19);
     $("#numToGuess").text(answer);
     $("#score").text(u)
 }
@@ -21,10 +21,7 @@ function checkScore(){
         updateAnswer();
         u=0
         $("#score").text(u)
-        updateAnswerC1();
-        updateAnswerC2();
-        updateAnswerC3();
-        updateAnswerC4();
+        updateCrystals();
     }
     else if(answer < u){
         loss= loss + 1;
@@ -32,29 +29,31 @@ function checkScore(){
         updateAnswer();
         u=0
         $("#score").text(u)
-        updateAnswerC1();
-        updateAnswerC2();
-        updateAnswerC3();
-        updateAnswerC4();
+        updateCrystals();
     }
 }
 
 function updateAnswerC1(){
-    answerC1 = parseInt([Math.floor(Math.random() * 13)]);
+    answerC1 = parseInt([Math.floor(Math.random() * 12)]+1);
 }
 function updateAnswerC2(){
-    answerC2 = parseInt([Math.floor(Math.random() * 13)]);
+    answerC2 = parseInt([Math.floor(Math.random() * 12)]+1);
 }
 function updateAnswerC3(){
-    answerC3 = parseInt([Math.floor(Math.random() * 13)]);
+    answerC3 = parseInt([Math.floor(Math.random() * 12)]+1);
 }
 function updateAnswerC4(){
-    answerC4 = parseInt([Math.floor(Math.random() * 13)]);
+    answerC4 = parseInt([Math.floor(Math.random() * 12)]+1);
 }
-updateAnswerC1();
-updateAnswerC2();
-updateAnswerC3();
-updateAnswerC4();
+
+function updateCrystals(){
+    updateAnswerC1();
+    updateAnswerC2();
+    updateAnswerC3();
+    updateAnswerC4();
+}
+
+updateCrystals();
 
 $("#crystal-1").on('click', function(){
     u= u+answerC1
