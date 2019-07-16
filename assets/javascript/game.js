3aed811cc7e96dc5b65c76eeafc3b7d8b2300a37
Geoffrey Bosch <49883803+geoffreybosch@users.noreply.github.com>
2019-07-15 21:07:00 -0700

    Updated game to resemble final funtionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,52 @@
 var win = 0;
 var loss = 0;
 var answer = "";
-var u = "";
+var u = 0;
 
 function updateAnswer(){
-    var rand = [Math.floor(Math.random() * 80)];
-    answer = parseInt(rand);
-    $("#score").text(answer);
+    answer = parseInt([Math.floor(Math.random() * 100)]);
+    $("#numToGuess").text(answer);
+    $("#score").text(u)
 }
 updateAnswer();
 
-function scoreCheck(){
-    $('losses').text = loss;
-    if (answer == "p"){
-        if (u=="s"){
-            win= win + 1;
-            $("wins").text(win);
-            updateAnswer();
-        }
+function checkScore(){
+    if (answer == u){  
+        win= win + 1;
+        $("wins").text(win);
+        updateAnswer();
+        u=0
+        $("#score").text(u)
+    }
+    else if(answer < u){
+        loss= loss + 1;
+        $("losses").text(loss);
+        updateAnswer();
+        u=0
+        $("#score").text(u)
     }
 }
+
+
+$("#crystal-1").on('click', function(){
+    u= u+8
+    $("#score").text(u)
+    checkScore()
+})
+$("#crystal-2").on('click', function(){
+    u= u+3
+    $("#score").text(u)
+    checkScore()
+})
+$("#crystal-3").on('click', function(){
+    u= u+5
+    $("#score").text(u)
+    checkScore()
+})
+$("#crystal-4").on('click', function(){
+    u= u+2
+    $("#score").text(u)
+    checkScore()
+})
+
+
